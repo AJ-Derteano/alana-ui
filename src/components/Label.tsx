bb478c6f30a97b9a55977275b1aa528523e56d29
AJ-Derteano <andresderteano@outlook.com>
2024-05-14 21:53:11 -0500

    feat: add label component

---
+++ src/components/Label.tsx
@@ -0,0 +1,51 @@
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+
+interface LabelProps {
+  children?: React.ReactNode;
+  type?: 'text' | 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6';
+}
+
+const Label = ({ children, type = 'text' }: LabelProps) => {
+  const getFontSize = () => {
+    return styles[type ?? 'text'];
+  };
+
+  return (
+    <View>
+      <Text style={[getFontSize()]}>{children}</Text>
+    </View>
+  );
+};
+
+export default Label;
+
+const styles = StyleSheet.create({
+  text: {
+    fontSize: 16,
+  },
+  h1: {
+    fontSize: 32,
+    marginBottom: 8,
+  },
+  h2: {
+    fontSize: 24,
+    marginBottom: 8,
+  },
+  h3: {
+    fontSize: 20,
+    marginBottom: 8,
+  },
+  h4: {
+    fontSize: 18,
+    marginBottom: 8,
+  },
+  h5: {
+    fontSize: 16,
+    marginBottom: 8,
+  },
+  h6: {
+    fontSize: 14,
+    marginBottom: 8,
+  },
+});
